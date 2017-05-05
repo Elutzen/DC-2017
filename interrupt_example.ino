89a90401f6143382144a08a608f7dd647db04384
Elutzen <Elutzen@gmail.com>
2017-05-05 17:15:40 -0500

    asdfsd

---
+++ interrupt_example.ino
@@ -0,0 +1,19 @@
+#define BUTTONPIN 2;
+#define buttonState 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(BUTTONPIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTONPIN),wall,CHANGE);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void wall(){
+  //turn the fuck around
+}
+
