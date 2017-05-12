0ae6617e7b51a037b0e947dd72e7289888420ff6
Elutzen <Elutzen@gmail.com>
2017-05-12 15:56:41 -0500

    audio working with complete system

--- audio_example/audio_example.ino
+++ audio_example/audio_example.ino
@@ -7,7 +7,19 @@
 
 
 // WAV files converted to code by wav2sketch
-#include "AudioSampleR2d2_scream.h"
+#include "AudioSampleR2d2_screamloud.h"
+#include "AudioSampleMarchshort.h"
+#include "AudioSampleB1.h"
+#include "AudioSampleB2.h"
+#include "AudioSampleB3.h"
+#include "AudioSampleB4.h"
+#include "AudioSampleB5.h"
+#include "AudioSampleB6.h"
+#include "AudioSampleB7.h"
+#include "AudioSampleB8.h"
+#include "AudioSampleCant.h"
+
+
 
 // Create the Audio components.  These should be created in the
 // order data flows, inputs/sources -> processing -> outputs
@@ -37,18 +49,19 @@ AudioConnection c9(mix2, 0, headphones, 1);
 AudioConnection c10(mix2, 0, dac0, 0);
 
 // Create an object to control the audio shield.
-// 
+//
 AudioControlSGTL5000 audioShield;
 
 // Bounce objects to read six pushbuttons (pins 0-5)
 //
 Bounce button0 = Bounce(0, 5);
-
+Bounce button1 = Bounce(1, 5);
 
 void setup() {
   // Configure the pushbutton pins for pullups.
   // Each button should connect from the pin to GND.
   pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
   Serial.begin(9600);
   // Audio connections require memory to work.  For more
   // detailed information, see the MemoryAndCpuUsage example
@@ -65,26 +78,71 @@ void setup() {
 
   // reduce the gain on mixer channels, so more than 1
   // sound can play simultaneously without clipping
-  mix1.gain(0, 0.4);
-  mix1.gain(1, 0.4);
-  mix1.gain(2, 0.4);
-  mix1.gain(3, 0.4);
-  mix2.gain(1, 0.4);
-  mix2.gain(2, 0.4);
+  mix1.gain(0, 1);
+  mix1.gain(1, 1);
+  mix1.gain(2, 1);
+  mix1.gain(3, 1);
+  mix2.gain(1, 1);
+  mix2.gain(2, 1);
+
+  attachInterrupt(0, scream, LOW);
+  attachInterrupt(1, march, LOW);
 }
 
 void loop() {
   // Update all the button objects
   button0.update();
-
+  button1.update();
   // When the buttons are pressed, just start a sound playing.
   // The audio library will play each sound through the mixers
   // so any combination can play simultaneously.
   //
-  if (button0.fallingEdge()) {
-    sound0.play(AudioSampleR2d2_scream);
+  if (button1.fallingEdge()) {
+    sound0.play(AudioSampleR2d2_screamloud);
   }
-
-
+  delay(2000);
+  int beep = random(1, 8);
+  if (!sound0.isPlaying()) {
+    switch (beep) {
+      case 1:
+        sound0.play(AudioSampleB1);
+        break;
+      case 2:
+        sound0.play(AudioSampleB2);
+        break;
+      case 3:
+        sound0.play(AudioSampleB3);
+        break;
+      case 4:
+        sound0.play(AudioSampleB4);
+        break;
+      case 5:
+        sound0.play(AudioSampleB5);
+        break;
+      case 6:
+        sound0.play(AudioSampleB6);
+        break;
+      case 7:
+        sound0.play(AudioSampleB7);
+        break;
+      case 8:
+        sound0.play(AudioSampleB8);
+        break;
+    }
+  }
 }
+
+void scream() {
+  delay(10);
+ if (digitalRead(0) == LOW) {
+  sound0.play(AudioSampleR2d2_screamloud);
+  }
+}
+void march() {
+  delay(10);
+ if (digitalRead(1) == LOW) {
+  sound0.play(AudioSampleMarchshort);
+ }
+}
+
 
